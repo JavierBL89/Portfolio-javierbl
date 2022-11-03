c5d4081bc43e2c77486d7db6b258f5e58b0f74d3
Javier <notri80@gmail.com>
2022-11-03 12:41:26 +0100

    Added layout text content into ME section

--- src/components/NavigationBar/Section.jsx
+++ src/components/NavigationBar/Section.jsx
@@ -19,7 +19,7 @@ function Section(){
                     </div>
                     <div className="section-content-container">
                         <p>Left my country 4 years ago in search of a better future.</p>
-                        <p>During this time i have self tought the language of english, how to play guitar
+                        <p>During this time i have self tought the language of english, how to play guitar,
                         and became a <strong>Junior Web Developer</strong> in this last 2 years.</p>
 
                         <div className="quote2-container ">
