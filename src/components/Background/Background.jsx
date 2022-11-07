e17ff0905951fa53ae367f159e4caddd7c0153d5
Javier <notri80@gmail.com>
2022-11-07 12:53:16 +0100

    Created Background component folder

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Background(){
+    return (
+        <section id="background-section">
+            <div className="container-fluid background-wraper" id="background-wraper">
+                <div className="container-fluid background-container" id="background-container">
+                    
+                </div>
+            </div>
+        </section>
+
+    
+    );
+};
+
+export default Background;
