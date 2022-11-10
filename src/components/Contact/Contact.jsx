09f5726aaee2de054584ca587cdd8f75c5245cda
Javier <notri80@gmail.com>
2022-11-10 11:31:30 +0100

    Adjusted Hero section and Navidation bar for TABLET SCREEN

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ import Form from "./Form";
 function Contact(){
     return(
         <section id="contact">
-            <div className="container-fluid g-0 contact-wraper" id="contact-wraper">
+            <div className="d-none container-fluid g-0 contact-wraper" id="contact-wraper">
                 <div className="container-fluid contact-container text-center">
                 <img src="https://img.icons8.com/clouds/80/null/add-message.png" alt="send-mesage"/>
                 <H3 className="mb-5" text="Give me a shout!"/>
