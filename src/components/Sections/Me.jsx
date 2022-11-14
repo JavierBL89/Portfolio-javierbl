44115faed1afc053138e7f3fe5f7d410379fb27f
Javier <notri80@gmail.com>
2022-11-15 00:51:43 +0100

    Adjusted Projects section design layout for LAPTOP and LARGE SCREENS

--- src/components/Sections/Me.jsx
+++ src/components/Sections/Me.jsx
@@ -6,15 +6,7 @@ import NavBarItem from "../NavigationBar/NavBarItem";
 function Me(){
     return (
             <section id="me-section">
-                <div className="container-fluid me-container">
-                {/* <div className="container-fluid secondary-navbar-wraper" id="secondary-navbar">
-                    <div className="container-fluid navbar-container">
-                        <NavBarItem className="nav-item me-link" id="me-link" text="ME"/>
-                        <NavBarItem className="nav-item projects-link" id="projects-link" text="PROJECTS"/>
-                        <NavBarItem className="nav-item background-link" id="background-link" text="BACKGROUND"/>
-                    </div>
-        
-                </div> */}
+                <div className="d-none container-fluid me-container">
                 <div className="section-content-container">
                     <p>Left my country 4 years ago in search of a better future.</p>
                     <p>During this time i have self tought the language of english, how to play guitar,
