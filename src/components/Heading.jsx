603c97e5a11503d24a2bc12f1442f709d9c016a9
Javier <notri80@gmail.com>
2022-11-14 11:14:02 +0100

    Adjusted Hero section tp LAPTOP SCREEN SIZE

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -4,12 +4,22 @@ import React from "react";
 function Heading(){
     return (
         <div className="container-fluid heading-wraper">
-            <div className="container-fluid heading-container">
-                <p className="line1-heading">Hey</p>
-                <p className="line2-heading">I'm Javier</p>
-                <div className="separator"></div>
-                <h1 className="profesion"><span className="junior">Junior</span> WEB DEVELOPER</h1>
-                <h2 className="subject">FULL STACK</h2>
+            <div className="container-fluid">
+              <div className="row ">
+                <div className="col-12 col-lg-6 heading-container">
+                    <p className="line1-heading">Hey</p>
+                    <p className="line2-heading">I'm Javier</p>
+                    <div className="separator"></div>
+                    <h1 className="profesion"><span className="junior">Junior</span> WEB DEVELOPER</h1>
+                    <h2 className="subject">FULL STACK</h2>
+                </div> 
+                <div className="col-12 col-lg-6">
+                   <div className="img-container">
+                   {/* <img src="https://res.cloudinary.com/dthlibbj7/image/upload/v1668343688/FORTALEZA_HOHENSALZBURG_20_iiqcmp.jpg" alt="javier"/> */}
+                   </div>
+                </div>
+              </div>
+                
             </div>
         </div>
     );
