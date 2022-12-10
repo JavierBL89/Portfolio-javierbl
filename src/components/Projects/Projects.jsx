abaaa0fc6c5ab95971cbb1ac01e6821a4df963b6
Javier <notri80@gmail.com>
2022-12-10 11:59:41 +0100

    Added project img nad technologies used test layout on mobile screen size

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,9 +6,9 @@ function Projects(){
     return (
         <section id="projects-section">
                     <div className="container fluid projects-wraper" id="projects-wraper">
-                        <div className="container-fluid projects-container hiden" id="projects-container">
+                        <div className="container-fluid projects-container g-0 hiden" id="projects-container">
         
-                            <div className="container-fluid project-card">
+                            <div className="container-fluid project-card g-0">
                         <div className="row g-0">
                             <div className="col-sm-12 col-lg-6">
                                <div className="project-card-head">
@@ -19,16 +19,15 @@ function Projects(){
                                         <div className="row">
                                             <div className="col-6 pe-0 project-tech-column">
                                               <img src="https://img.icons8.com/color/25/null/python--v1.png" alt="python-logo"/>
-                                              <img src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/null/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-bold-tal-revivo.png" alt="django-logo"/>
-                                              <img src="https://img.icons8.com/color/25/null/python--v1.png" alt="python-logo"/>
-                                              <img src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/null/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-bold-tal-revivo.png" alt="django-logo"/>
-                                            
+                                              <img src="https://img.icons8.com/ios/25/null/javascript--v1.png" alt="javascript-logo"/>
+                                              <img src="https://img.icons8.com/color/25/null/stripe.png" alt="stripe-logo"/>
+                                              <img src="https://img.icons8.com/color/28/null/bootstrap.png" alt="bootstrap-logo"/>                                             
                                             </div>
                                         <div className="col-6 ps-0 project-tech-column">
-                                              <img src="https://img.icons8.com/color/25/null/python--v1.png" alt="python-logo"/>
                                               <img src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/null/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-bold-tal-revivo.png" alt="django-logo"/>
-                                              <img src="https://img.icons8.com/color/25/null/python--v1.png" alt="python-logo"/>
-                                              <img src="https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/null/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-bold-tal-revivo.png" alt="django-logo"/>
+                                              <img src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-jquery-is-a-javascript-library-designed-to-simplify-html-logo-shadow-tal-revivo.png" alt="jQuery-logo"/>
+                                              <img src="https://img.icons8.com/color/25/null/amazon-web-services.png" alt="aws-logo"/>                                             
+                                              <img src="https://img.icons8.com/ios-glyphs/25/null/filled-sent.png" alt="email-send"/>
                                             </div>
                                         </div>
                                     </div>
