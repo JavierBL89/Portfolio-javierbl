1658361ead3ca898d33193b0a6099ba54ab0bcd3
Javier <notri80@gmail.com>
2023-01-09 00:13:54 +0100

    Aumented contact form wrapper size for mobile screen

--- src/dataProjects.js
+++ src/dataProjects.js
@@ -34,7 +34,7 @@ export const projects = [
     //     },
         {
             title: "Mindfullness & Meditation",
-            url: "",
+            url: "https://javierbl89.github.io/Mindfulness-Portfolio-Project-Code-Institute/",
             image: "https://res.cloudinary.com/dthlibbj7/image/upload/v1673220273/image_2_dpeiil.png",
             description: `oncdjdnjvidnivjnsdjivnsji kdncsocjnsjon dcnodjsncosncon odcnsdocnso
                          oncd jdnjvid niv  jnsdji vnsji kdncsocjnsjon dcn odj sncos ncon odcns do cnso
@@ -64,20 +64,20 @@ export const projects = [
                              {icon: "https://img.icons8.com/color/25/null/amazon-web-services.png", name: "email-send"},
                           ]
                 },
-                {
-                    title: "Mindfullness & Meditation",
-                    url: "https://javierbl89.github.io/Mindfulness-Portfolio-Project-Code-Institute/",
-                    image: "../../src/images/Captura_de_pantalla_(665).png",
-                    description: `oncdjdnjvidnivjnsdjivnsji kdncsocjnsjon dcnodjsncosncon odcnsdocnso
-                                 oncd jdnjvid niv  jnsdji vnsji kdncsocjnsjon dcn odj sncos ncon odcns do cnso
-                                 onc djdnjvidni vjn sdjivn sji kdncs ocjnsjon dcnodj sncosncon odcnsdocnso`,
-                                 technology : [ {icon: "https://img.icons8.com/color/25/null/python--v1.png", name: "python-logo"}, 
-                                 {icon: "https://img.icons8.com/ios/25/null/javascript--v1.png", name: "javascript-logo"},
-                                 {icon: "https://img.icons8.com/color/25/null/stripe.png", name: "stripe-logo"},
-                                 {icon: "https://img.icons8.com/color/28/null/bootstrap.png", name: "bootstrap-logo"},
-                                 {icon: "https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/null/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-bold-tal-revivo.png", name: "django-logo"},
-                                 {icon: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-jquery-is-a-javascript-library-designed-to-simplify-html-logo-shadow-tal-revivo.png", name: "jQuery-logo"},
-                                 {icon: "https://img.icons8.com/color/25/null/amazon-web-services.png", name: "email-send"},
-                              ]
-                    },
+                // {
+                //     title: "Mindfullness & Meditation",
+                //     url: "https://javierbl89.github.io/Mindfulness-Portfolio-Project-Code-Institute/",
+                //     image: "../../src/images/Captura_de_pantalla_(665).png",
+                //     description: `oncdjdnjvidnivjnsdjivnsji kdncsocjnsjon dcnodjsncosncon odcnsdocnso
+                //                  oncd jdnjvid niv  jnsdji vnsji kdncsocjnsjon dcn odj sncos ncon odcns do cnso
+                //                  onc djdnjvidni vjn sdjivn sji kdncs ocjnsjon dcnodj sncosncon odcnsdocnso`,
+                //                  technology : [ {icon: "https://img.icons8.com/color/25/null/python--v1.png", name: "python-logo"}, 
+                //                  {icon: "https://img.icons8.com/ios/25/null/javascript--v1.png", name: "javascript-logo"},
+                //                  {icon: "https://img.icons8.com/color/25/null/stripe.png", name: "stripe-logo"},
+                //                  {icon: "https://img.icons8.com/color/28/null/bootstrap.png", name: "bootstrap-logo"},
+                //                  {icon: "https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/null/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-bold-tal-revivo.png", name: "django-logo"},
+                //                  {icon: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-jquery-is-a-javascript-library-designed-to-simplify-html-logo-shadow-tal-revivo.png", name: "jQuery-logo"},
+                //                  {icon: "https://img.icons8.com/color/25/null/amazon-web-services.png", name: "email-send"},
+                //               ]
+                //     },
 ]
