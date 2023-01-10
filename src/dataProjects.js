c6da9a5510bfe796386c625228ab1e3247481496
Javier <notri80@gmail.com>
2023-01-10 15:12:57 +0100

    Added projects description, new tech icons, and Oryopoxo project

--- src/dataProjects.js
+++ src/dataProjects.js
@@ -13,15 +13,44 @@ export const projects = [
                        {icon: "https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/null/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-bold-tal-revivo.png", name: "django-logo"},
                        {icon: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-jquery-is-a-javascript-library-designed-to-simplify-html-logo-shadow-tal-revivo.png", name: "jQuery-logo"},
                        {icon: "https://img.icons8.com/color/25/null/amazon-web-services.png", name: "email-send"},
+                       {icon: "https://img.icons8.com/color/25/null/html-5--v1.png", name: "email-send"},
+                       {icon: "https://img.icons8.com/color/25/null/css3.png", name: "email-send"},
                     ]
                     },
+                    {
+                        title: "Restaurant application",
+                        url: "https://oryo-poxo.onrender.com/",
+                        image: "https://res.cloudinary.com/dthlibbj7/image/upload/v1673356964/Captura_de_pantalla_683_g4dhne.png",
+                        description: `Back-end application powered by Python, class based views and django models. Jquery and AnimateCSS frameworks is used for client side. Users can make reservations and it checks for double reservations`,
+                                     technology : [ {icon: "https://img.icons8.com/color/25/null/python--v1.png", name: "python-logo"}, 
+                                     {icon: "https://img.icons8.com/ios/25/null/javascript--v1.png", name: "javascript-logo"},
+                                     {icon: "https://img.icons8.com/color/28/null/bootstrap.png", name: "bootstrap-logo"},
+                                     {icon: "https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/null/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-bold-tal-revivo.png", name: "django-logo"},
+                                     {icon: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-jquery-is-a-javascript-library-designed-to-simplify-html-logo-shadow-tal-revivo.png", name: "jQuery-logo"},
+                                     {icon: "https://img.icons8.com/color/25/null/amazon-web-services.png", name: "email-send"},
+                                     {icon: "https://img.icons8.com/color/25/null/html-5--v1.png", name: "email-send"},
+                                     {icon: "https://img.icons8.com/color/25/null/css3.png", name: "email-send"},
+                                  ]
+                        },
+                    // {
+                    //     title: "Blog",
+                    //     url: "https://secrets-yj3o.onrender.com/",
+                    //     image: "https://res.cloudinary.com/dthlibbj7/image/upload/v1673356964/Captura_de_pantalla_683_g4dhne.png",
+                    //     description: `Simple blog in which only registered users are allowed to submit a secret. This is a full stack application powered by node.js and javascript`,
+                    //                  technology : [ {icon: "https://img.icons8.com/color/25/null/python--v1.png", name: "python-logo"}, 
+                    //                  {icon: "https://img.icons8.com/ios/25/null/javascript--v1.png", name: "javascript-logo"},
+                    //                  {icon: "https://img.icons8.com/color/25/null/stripe.png", name: "stripe-logo"},
+                    //                  {icon: "https://img.icons8.com/color/28/null/bootstrap.png", name: "bootstrap-logo"},
+                    //                  {icon: "https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/null/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-bold-tal-revivo.png", name: "django-logo"},
+                    //                  {icon: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-jquery-is-a-javascript-library-designed-to-simplify-html-logo-shadow-tal-revivo.png", name: "jQuery-logo"},
+                    //                  {icon: "https://img.icons8.com/color/25/null/amazon-web-services.png", name: "email-send"},
+                    //               ]
+                    //     },
     // {
-    //     title: "Ecommerce Store",
-    //     url: "",
-    //     image: "../../src/images/Captura_de_pantalla_(665).png",
-    //     description: `oncdjdnjvidnivjnsdjivnsji kdncsocjnsjon dcnodjsncosncon odcnsdocnso
-    //                  oncd jdnjvid niv  jnsdji vnsji kdncsocjnsjon dcn odj sncos ncon odcns do cnso
-    //                  onc djdnjvidni vjn sdjivn sji kdncs ocjnsjon dcnodj sncosncon odcnsdocnso`,
+    //     title: "Blog with authentication",
+    //     url: "https://secrets-yj3o.onrender.com/",
+    //     image: "https://res.cloudinary.com/dthlibbj7/image/upload/v1673344348/Captura_de_pantalla_682_aidtda.png",
+    //     description: `Simple blog in which only registered users are allowed to submit a secret. This is a full stack application powered by node.js and javascript`,
     //                  technology : [ {icon: "https://img.icons8.com/color/25/null/python--v1.png", name: "python-logo"}, 
     //                  {icon: "https://img.icons8.com/ios/25/null/javascript--v1.png", name: "javascript-logo"},
     //                  {icon: "https://img.icons8.com/color/25/null/stripe.png", name: "stripe-logo"},
@@ -31,35 +60,42 @@ export const projects = [
     //                  {icon: "https://img.icons8.com/color/25/null/amazon-web-services.png", name: "email-send"},
     //               ]
     //     },
+        // {
+        //     title: "Blog",
+        //     url: "https://secrets-yj3o.onrender.com/",
+        //     image: "https://res.cloudinary.com/dthlibbj7/image/upload/v1673356964/Captura_de_pantalla_683_g4dhne.png",
+        //     description: `Simple blog in which only registered users are allowed to submit a secret. This is a full stack application powered by node.js and javascript`,
+        //                  technology : [ {icon: "https://img.icons8.com/color/25/null/python--v1.png", name: "python-logo"}, 
+        //                  {icon: "https://img.icons8.com/ios/25/null/javascript--v1.png", name: "javascript-logo"},
+        //                  {icon: "https://img.icons8.com/color/25/null/stripe.png", name: "stripe-logo"},
+        //                  {icon: "https://img.icons8.com/color/28/null/bootstrap.png", name: "bootstrap-logo"},
+        //                  {icon: "https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/null/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-bold-tal-revivo.png", name: "django-logo"},
+        //                  {icon: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-jquery-is-a-javascript-library-designed-to-simplify-html-logo-shadow-tal-revivo.png", name: "jQuery-logo"},
+        //                  {icon: "https://img.icons8.com/color/25/null/amazon-web-services.png", name: "email-send"},
+        //               ]
+        //     },
         {
             title: "Blog Mindfullness & Meditation",
             url: "https://javierbl89.github.io/Mindfulness-Portfolio-Project-Code-Institute/",
             image: "https://res.cloudinary.com/dthlibbj7/image/upload/v1673315026/Captura_de_pantalla_676_lbnusq.png",
             description: `Beautiful blog with an excellent color pallet, rich on information and well structured.
-            This project can be largely scalabled by adding some backend features. Build with just HTML and CSS in combination with Bootstrap for grid`,
-                         technology : [ {icon: "https://img.icons8.com/color/25/null/python--v1.png", name: "python-logo"}, 
-                         {icon: "https://img.icons8.com/ios/25/null/javascript--v1.png", name: "javascript-logo"},
-                         {icon: "https://img.icons8.com/color/25/null/stripe.png", name: "stripe-logo"},
+            This project can be largely scalabled by adding some backend features. Build with just HTML and CSS in combination with the power of Bootstrap`,
+                         technology : [ 
+                         {icon: "https://img.icons8.com/color/25/null/html-5--v1.png", name: "email-send"},
+                         {icon: "https://img.icons8.com/color/25/null/css3.png", name: "email-send"},
                          {icon: "https://img.icons8.com/color/28/null/bootstrap.png", name: "bootstrap-logo"},
-                         {icon: "https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/null/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-bold-tal-revivo.png", name: "django-logo"},
-                         {icon: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-jquery-is-a-javascript-library-designed-to-simplify-html-logo-shadow-tal-revivo.png", name: "jQuery-logo"},
-                         {icon: "https://img.icons8.com/color/25/null/amazon-web-services.png", name: "email-send"},
                       ]
             },
             {
                 title: "Trivia Quizz Game",
                 url: "https://javierbl89.github.io/QUIZZ-QUIZZ-Trivia-Quizz-Game-Code-Institute-Project-2/",
                 image: "https://res.cloudinary.com/dthlibbj7/image/upload/v1673315624/Captura_de_pantalla_681_fwycrd.png",
-                description: `oncdjdnjvidnivjnsdjivnsji kdncsocjnsjon dcnodjsncosncon odcnsdocnso
-                             oncd jdnjvid niv  jnsdji vnsji kdncsocjnsjon dcn odj sncos ncon odcns do cnso
-                             onc djdnjvidni vjn sdjivn sji kdncs ocjnsjon dcnodj sncosncon odcnsdocnso`,
-                             technology : [ {icon: "https://img.icons8.com/color/25/null/python--v1.png", name: "python-logo"}, 
+                description: `Classic trivia game powered by JavaScript making used of modern EJS6 features such as template literals as well as constructor functions. The game shuffle questions and moves into higher levels of difficulty as the user progresses. Application build with the basics of HTML, CSS and JavaSript `,
+                             technology : [ 
                              {icon: "https://img.icons8.com/ios/25/null/javascript--v1.png", name: "javascript-logo"},
-                             {icon: "https://img.icons8.com/color/25/null/stripe.png", name: "stripe-logo"},
                              {icon: "https://img.icons8.com/color/28/null/bootstrap.png", name: "bootstrap-logo"},
-                             {icon: "https://img.icons8.com/external-tal-revivo-bold-tal-revivo/24/null/external-django-a-high-level-python-web-framework-that-encourages-rapid-development-logo-bold-tal-revivo.png", name: "django-logo"},
-                             {icon: "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-jquery-is-a-javascript-library-designed-to-simplify-html-logo-shadow-tal-revivo.png", name: "jQuery-logo"},
-                             {icon: "https://img.icons8.com/color/25/null/amazon-web-services.png", name: "email-send"},
+                             {icon: "https://img.icons8.com/color/25/null/html-5--v1.png", name: "email-send"},
+                             {icon: "https://img.icons8.com/color/25/null/css3.png", name: "email-send"},
                           ]
                 },
                 // {
