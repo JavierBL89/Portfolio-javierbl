e16638d9ad78aafda84b8fa6a948d268a2371de9
Javier <notri80@gmail.com>
2023-01-12 22:23:51 +0100

    Added favicon

--- src/components/NavigationBar/MainNavbar.jsx
+++ src/components/NavigationBar/MainNavbar.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
 import NavBarItem from "./NavBarItem";
-import Me from "../Me/Me";
-import Projects from "../Projects/Projects";
-import Background from "../Background/Background";
 import Section from "../Section";
 import show from "../../scripts/showSection";
-import Contact from "../Contact/Contact";
 
 function MainNavbar(){
 
@@ -21,7 +17,7 @@ function MainNavbar(){
     return(
             <div className="container-fluid main-navbar-wraper" id="main-navbar">
                 <div className="container-fluid main-navbar-container" id="main-navbar-container">
-                    <NavBarItem showSection={showSection} className="nav-item me-link" id="me-link" text="ME"/>
+                    <NavBarItem showSection={showSection} link="#me-section" className="nav-item me-link" id="me-link" text="ME"/>
                     <NavBarItem showSection={showSection} link="#projects-section" className="nav-item projects-link" id="projects-link" text="PROJECTS"/>
                     <NavBarItem showSection={showSection} link="#background-section" className="nav-item background-link" id="background-link" text="BACKGROUND"/>
                 </div>
